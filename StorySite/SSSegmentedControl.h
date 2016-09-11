91e409d399a815e849e1085d8ac7fb2642702187
Vicc Alexander <viccalexander@gmail.com>
2016-09-11 01:26:48 -0700

    Added SSSegmentedControl

---
+++ StorySite/SSSegmentedControl.h
@@ -0,0 +1,15 @@
+//
+//  SSSegmentedControl.h
+//  StorySite
+//
+//  Created by Vicc Alexander on 9/11/16.
+//  Copyright © 2016 StorySiteTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SSSegmentedControl : UIViewController
+
+@end
+
+
---
+++ StorySite/SSSegmentedControl.m
@@ -0,0 +1,13 @@
+//
+//  SSSegmentedControl.m
+//  StorySite
+//
+//  Created by Vicc Alexander on 9/11/16.
+//  Copyright © 2016 StorySiteTeam. All rights reserved.
+//
+
+#import "SSSegmentedControl.h"
+
+@implementation SSSegmentedControl
+
+@end
